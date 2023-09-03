c8ee34d97ed13cd188fea184986286a0b7932933
a <a>
2023-09-03 17:49:05 +0700

    add vue-router

---
+++ src/Login.vue
@@ -0,0 +1,94 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <br>
+    <input v-model="login" placeholder="login" />
+    <br>
+    <input v-if="isPassword" v-model="password" type="password" placeholder="password"/>
+    <i
+      class="password-toggle"
+      :class="{'fas fa-eye': isPassword, 'fas fa-eye-slash': !isPassword}"
+      @click="togglePasswordVisibility"
+    ></i>
+    <br>
+    <br>
+    <button class="button" @click="signInEvent">sign in</button>
+    <button class="button" @click="signUpEvent">sign up</button>
+    <br>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      login: '',
+      password: '',
+      isPassword: true
+    }
+  },
+  methods: {
+    signInEvent() {
+      if (this.login == '' || this.password == '') {
+        console.log('login and password cannot be empty');
+        return;
+      }
+
+      console.log('Sign In clicked!');
+      this.$router.push('/dashboard');
+    },
+    signUpEvent() {
+      console.log('Sign Up clicked!');
+    },
+    togglePasswordVisibility() {
+      this.isPassword = !this.isPassword;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px; 
+}
+
+.button {
+  margin-right: 10px;
+  padding: 10px 20px;
+  background-color: #15508e;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+}
+
+.password-toggle {
+  position: absolute;
+  top: 50%;
+  right: 10px;
+  transform: translateY(-50%);
+  cursor: pointer;
+}
+
+.password-toggle i {
+  display: none;
+}
+
+.password-toggle i.fa-eye {
+  display: none;
+}
+
+.password-toggle i.fa-eye-slash {
+  display: none;
+}
+
+.eye-icon {
+  width: 16px;
+  height: auto;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// main.js
+import { createApp } from 'vue';
+import * as VueRouter from 'vue-router';
+import Login from './Login.vue';
+import Dashboard from './DashboardPage.vue';
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
+  { path: '/dashboard', component: Dashboard }
+];
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHistory(),
+  routes
+});
+
+createApp()
+  .use(router)
+  .mount('#app');
