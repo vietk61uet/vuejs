ab3d5e0fdb0b4a4849b16771cd4a38a43c79e309
vietk61uet <phanquocviet2209@gmail.com>
2023-09-10 21:58:00 +0700

    add image assets for product

--- src/DashboardPage.vue
+++ src/DashboardPage.vue
@@ -1,109 +1,139 @@
 <template>
-  <div class="product-list">
-    <div class="product-row" v-for="(row, index) in productRows" :key="index">
-      <div class="product" v-for="product in row" :key="product.title">
-        <div class="product-details">
-          <div class="product-image">
-            <img :src="getImageUrl(product.image)" :alt="product.title">
-            <h2>{{ product.title }}</h2>
-            <p>{{ product.description }}</p>
-            <p>Price: {{ product.price }} USD</p>
-            <button @click="addToCart">Add to Cart</button>
-          </div>
-        </div>
+  <div class="shop">
+    <header>
+      <h1>Shop Online</h1>
+    </header>
+    <main>
+      <div class="product-list">
+        <div v-for="product in products" :key="product.id" class="product">
+          <div class="product-image-wrapper" 
+          @mouseenter="setHovered(product, true)"
+          @mouseleave="setHovered(product, false)">
+          <img :src="product.image" :alt="product.title" class="product-image">
+          <h2>{{ product.title }}</h2>
+          <p>Price: {{ product.price }} USD</p>
+          <button v-show="product.hovered" @click="addToCart(product)" class="add-to-cart-button">Add to Cart</button>
         </div>
       </div>
-    </div>
+      </div>
+    </main>
+    <footer>
+      <p>Cart: {{ cartItems.length }} items</p>
+    </footer>
+  </div>
 </template>
-
-<style scoped>
-.product-list {
-  width: 900px;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  margin: 0 auto; /* Căn giữa theo chiều ngang */
-}
-
-.product-row {
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  margin-bottom: 20px;
-}
-
-.product-details {
-  display: flex;
-}
-
-.product {
-  flex-basis: 300px; /* Độ rộng cố định cho mỗi sản phẩm */
-  flex-grow: 1; /* Cho phép sản phẩm mở rộng nếu không đủ không gian */
-}
-
-.product-image img {
-  max-width: 200px;
-  max-height: 200px;
-  width: auto;
-  height: auto;
-}
-</style>
 
 <script>
 export default {
-  name: 'ProductList',
   data() {
     return {
       products: [
         {
-          title: 'Example Product 1',
-          description: 'This is the first example product a a a a a a   a  a.  â a a a',
+          id: 1,
+          title: 'Áo thun Huế',
           price: 10,
-          image: 'quang_tri.jpg'
+          image: require("./assets/hue.jpg"),
+          hovered: false
         },
         {
-          title: 'Example Product 2',
-          description: 'This is the second example product.',
-          price: 20,
-          image: 'quang_tri.jpg'
+          id: 2,
+          title: 'Áo thun Quảng Trị',
+          price: 15,
+          image: require("./assets/quang_tri.jpg"),
+          hovered: false
         },
         {
-          title: 'Example Product 2',
-          description: 'This is the second example product.',
-          price: 20,
-          image: 'quang_tri.jpg'
+          id: 3,
+          title: 'Áo thun Vũng Tàu',
+          price: 15,
+          image: require("./assets/vung_tau.jpg"),
+          hovered: false
         },
         {
-          title: 'Example Product 2',
-          description: 'This is the second example product.',
-          price: 20,
-          image: 'quang_tri.jpg'
+          id: 3,
+          title: 'Áo thun Vũng Tàu',
+          price: 15,
+          image: require("./assets/vung_tau.jpg"),
+          hovered: false
+        },
+        {
+          id: 3,
+          title: 'Áo thun Vũng Tàu',
+          price: 15,
+          image: require("./assets/vung_tau.jpg"),
+          hovered: false
         },
         // Thêm các sản phẩm khác vào đây
-      ]
+      ],
+      cartItems: []
     };
   },
-  computed: {
-    productRows() {
-      const rows = [];
-      const productsPerRow = 3;
-      let rowIndex = 0;
-
-      for (let i = 0; i < this.products.length; i += productsPerRow) {
-        const row = this.products.slice(i, i + productsPerRow);
-        rows[rowIndex] = row;
-        rowIndex++;
-      }
-
-      return rows;
-    }
-  },
   methods: {
-    getImageUrl(image) {
-      return require(`@/assets/${image}`);
+    addToCart(product) {
+      this.cartItems.push(product);
     },
-    addToCart() {
+    setHovered(product, isHovered) {
+      product.hovered = isHovered;
+    },
+    handleImageClick(product) {
+      if (product.hovered) {
+        this.addToCart(product);
+      }
     }
   }
 };
 </script>
+
+<style>
+/* CSS cho các phần tử trong trang web */
+.shop {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+header {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.product-list {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+}
+
+.product {
+  text-align: center;
+  position: relative;
+}
+
+.product-image-wrapper {
+  position: relative;
+  display: inline-block;
+}
+
+.product-image {
+  width: 200px;
+  height: 200px;
+  margin-bottom: 10px;
+}
+
+.add-to-cart-button {
+  display: none;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+}
+
+.product:hover .add-to-cart-button {
+  display: block;
+}
+
+footer {
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
